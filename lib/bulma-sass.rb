356b7b5c12bc945f8aa3c0b9e184c78f9996503c
bananaappletw <bananaappletw@gmail.com>
2017-11-03 22:09:29 +0800

    Support sprockets

--- lib/bulma-sass.rb
+++ lib/bulma-sass.rb
@@ -1,7 +1,40 @@
 require "bulma/sass/version"
-require "bulma/sass/engine"
 
 module Bulma
   module Sass
+    class << self
+      def load!
+        if defined?(::Rails)
+          require "bulma/sass/engine"
+        elsif defined?(::Sprockets)
+          Sprockets.append_path(stylesheets_path)
+        end
+        configure_sass
+      end
+
+      # Paths
+      def gem_path
+        @gem_path ||= File.expand_path('..', File.dirname(__FILE__))
+      end
+
+      def templates_path
+        File.join(gem_path, 'templates')
+      end
+
+      def assets_path
+        @assets_path ||= File.join(gem_path, 'app', 'assets')
+      end
+
+      def stylesheets_path
+        File.join(assets_path, 'stylesheets')
+      end
+
+      def configure_sass
+        require 'sass'
+        ::Sass.load_paths << stylesheets_path
+      end
+
+    end
   end
 end
+Bulma::Sass.load!
