165e0afb6ff01eebb2234f56faae3660c20dc739
bananaappletw <bananaappletw@gmail.com>
2019-07-20 16:11:48 +0800

    Add release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Enter version"
+read version
+sed -i "s/VERSION = \".*\"/VERSION = \"$version\"/g" lib/bulma/sass/version.rb
+rm -rf app/assets/stylesheets/*
+rm -rf build/
+git clone -b "$version" https://github.com/jgthms/bulma build/
+mv build/bulma.sass app/assets/stylesheets/
+mv build/sass/ app/assets/stylesheets/
+rm -rf build/
+git add .
+git commit -m "Release v$version"
+git tag -a "v$version" -m "v$version"
+gem build bulma-sass.gemspec
+gem push "bulma-sass-$version.gem"
